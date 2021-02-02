f556f6b9ff68c5f0c24ec809bed58b7cc6703ae1
Cakelover-png <NamelessAlien1@gmail.com>
2021-02-02 18:39:41 +0400

    Optimized washer filter correctly

--- management/views.py
+++ management/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.db.models import Sum, Count, Q
+from decimal import Decimal
+from django.db.models import Sum, Count, Q, F, ExpressionWrapper, DecimalField
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
-import datetime
-from .models import CarWash
+from .models import CarWash, Order
 
 
 def homepage(request):
@@ -19,9 +19,11 @@ def car_washes(request):
 
 def car_wash_detail(request, cw_pk):
     car_wash = get_object_or_404(CarWash, pk=cw_pk)
-    washed_cars = car_wash.washer_set.annotate(
-        orders_count=Count('order', filter=Q(order__finish_time__isnull=False)),
-    ).aggregate(Sum('orders_count'))['orders_count__sum']
+    # washed_cars = car_wash.washer_set.annotate(
+    #     orders_count=Count('order', filter=Q(order__finish_time__isnull=False)),
+    # ).aggregate(Sum('orders_count'))['orders_count__sum']
+    washed_cars = Order.objects.filter(finish_time__isnull=False,
+                                       washer__car_wash_id=car_wash.id).count()
     washed_cars = washed_cars if washed_cars else 0
     return render(request, 'management/car_wash_detail.html',
                   context={'car_wash': car_wash,
@@ -31,32 +33,34 @@ def car_wash_detail(request, cw_pk):
 
 def washers(request, cw_pk):
     car_wash = get_object_or_404(CarWash, pk=cw_pk)
-    washers = car_wash.washer_set.all()
-    washers_dict = {}
 
-    for washer in washers:
-        all_finished = washer.order_set.filter(
-            finish_time__isnull=False)
-        yearly_finished = all_finished.filter(
-            finish_time__gt=timezone.now()-datetime.timedelta(days=365))
-        monthly_finished = yearly_finished.filter(
-            finish_time__gt=timezone.now()-datetime.timedelta(days=30))
-        weakly_finished = monthly_finished.filter(
-            finish_time__gt=timezone.now()-datetime.timedelta(days=7))
-        weakly_pay = weakly_finished.aggregate(Sum('price'))['price__sum']
-        monthly_pay = monthly_finished.aggregate(Sum('price'))['price__sum']
-        yearly_pay = yearly_finished.aggregate(Sum('price'))['price__sum']
-        washers_dict[washer.id] = {
-            'weakly_washed': weakly_finished.count(),
-            'weakly_pay': round(weakly_pay/2, 2) if weakly_pay else 0.0,
-            'monthly_washed': monthly_finished.count(),
-            'monthly_pay': round(monthly_pay/2, 2) if monthly_pay else 0.0,
-            'yearly_washed': yearly_finished.count(),
-            'yearly_pay': round(yearly_pay/2, 2) if yearly_pay else 0.0,
-            'all_washed': all_finished.count(),
-        }
+    washers = car_wash.washer_set.annotate(
+        washer_earned_money=ExpressionWrapper(
+            F('order__price')/Decimal('2.00'), output_field=DecimalField())
+    ).annotate(
+        all_washed=Count('order', filter=Q(order__finish_time__isnull=False)),
+
+        all_pay=Sum('washer_earned_money', filter=Q(order__finish_time__isnull=False)),
+
+        yearly_washed=Count('order', filter=Q(
+            order__finish_time__gte=timezone.now() - timezone.timedelta(days=356))),
+
+        yearly_pay=Sum('washer_earned_money', filter=Q(
+            order__finish_time__gte=timezone.now() - timezone.timedelta(days=365))),
+
+        monthly_washed=Count('order', filter=Q(
+            order__finish_time__gte=timezone.now() - timezone.timedelta(days=30))),
+
+        monthly_pay=Sum('washer_earned_money', filter=Q(
+            order__finish_time__gte=timezone.now() - timezone.timedelta(days=30))),
+
+        weekly_washed=Count('order', filter=Q(
+            order__finish_time__gte=timezone.now() - timezone.timedelta(days=7))),
+
+        weekly_pay=Sum('washer_earned_money', filter=Q(
+            order__finish_time__gte=timezone.now() - timezone.timedelta(days=7))),
+    )
 
     return render(request, 'management/washers.html',
-                  context={'washers': washers,
-                           'washers_dict': washers_dict}
+                  context={'washers': washers, }
                   )
